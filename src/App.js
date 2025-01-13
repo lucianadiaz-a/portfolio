48784a75eb183fb26a506f4d712e12d504721d2f
luciana-mdi <luciana@mormedi.com>
2025-01-13 16:46:18 +0100

    base landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Project from './pages/Project';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          {/* Home Page */}
+          <Route path="/" element={<Home />} />
+          {/* Dynamic Project Pages */}
+          <Route path="/projects/:id" element={<Project />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
