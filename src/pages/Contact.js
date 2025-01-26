a45c5a6b6d9b1e9a8c31007fc3a865cc66d8e7fc
Luciana Diaz Albadan <84077814+lucianadiaz-a@users.noreply.github.com>
2025-01-26 16:56:27 +0100

    v2.1 cleaned up + project added (#3)

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import './css/Contact.css';
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact-container">
       <h2>Contact</h2>
-      <p>i´m constantly online but mostly on LinkedIn</p>
-      <p>shoot me a dm if you want to chat</p>
+      <p>
+        I’m constantly online but mostly on{' '}
+        <a
+          href="https://www.linkedin.com/in/luciana-diaz-albadan/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="contact-link"
+        >
+          LinkedIn
+        </a>
+      </p>
+      <p>Shoot me a DM if you want to chat</p>
     </div>
   );
 };
